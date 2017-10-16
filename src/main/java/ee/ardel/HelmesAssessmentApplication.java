d037b2982f256e09713b1feb24b4bb9a7cee25df
janis ardel <janis.ardel@netgroup.ee>
2017-10-17 00:23:08 +0300

    Fix project POM and remove unused imports

--- src/main/java/ee/ardel/HelmesAssessmentApplication.java
+++ src/main/java/ee/ardel/HelmesAssessmentApplication.java
@@ -2,14 +2,6 @@ package ee.ardel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.ExpiringSession;
-import org.springframework.session.MapSessionRepository;
-import org.springframework.session.SessionRepository;
-import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
-import org.springframework.session.web.http.HttpSessionStrategy;
 
 @SpringBootApplication
 public class HelmesAssessmentApplication {
